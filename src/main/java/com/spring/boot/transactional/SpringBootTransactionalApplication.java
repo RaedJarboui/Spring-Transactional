e72676c2cc81f02ca73c31af3fe3e2ce9d7f6076
RaedJarboui <raedjarboui1998@gmail.com>
2024-10-31 17:55:02 +0100

    add transactional to bookflightTicket method

--- src/main/java/com/spring/boot/transactional/SpringBootTransactionalApplication.java
+++ src/main/java/com/spring/boot/transactional/SpringBootTransactionalApplication.java
@@ -2,8 +2,10 @@ package com.spring.boot.transactional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBootTransactionalApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/spring/boot/transactional/service/FlightBookingService.java
+++ src/main/java/com/spring/boot/transactional/service/FlightBookingService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.boot.transactional.dto.FlightBookingAcknowledgement;
 import com.spring.boot.transactional.dto.FlightBookingRequest;
@@ -19,9 +20,15 @@ public class FlightBookingService {
 	@Autowired
 	private PaymentInfoRepository paymentInfoRepo;
 
+	@Transactional // we can add those params inside transactional annotation : Transaction
+					// Rollback - Transaction Logging - ReadOnly Transactions - Isolation Level
 	public FlightBookingAcknowledgement bookFlightTicket(FlightBookingRequest flightRequest) {
 		passengerInfoRepo.save(flightRequest.getPassengerInfo());
-		if (flightRequest.getPassengerInfo().getFare() < 2000.0)
+		if (flightRequest.getPassengerInfo().getFare() < 2000.0) // problem in case when theres exception : we have
+																	// already passengerInfo already stored in DB, but
+																	// theres no use when passengerInfo is stored and
+																	// paymentInfo is Not ! that is why we use
+																	// transactional to avoid this kind of scenarios
 			throw new PassengerInfoFlareException("passenger info flare should be lower than 2000");
 		paymentInfoRepo.save(flightRequest.getPaymentInfo());
 		return new FlightBookingAcknowledgement("success", flightRequest.getPassengerInfo().getFare(),
